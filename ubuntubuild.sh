37933dad9707bd8caa5982c770febb1306a33738
Russell VanderMey <russell@vandermey.us>
2026-03-07 09:51:44 -0500

    Install gh

--- ubuntubuild.sh
+++ ubuntubuild.sh
@@ -2,9 +2,15 @@
 set -euo pipefail
 
 
-echo "Installing rpm tools..."
+echo "Installing build tools..."
 apt-get update -y
-apt-get install -y rpm
+apt-get install -y rpm curl
+
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | dd of=/usr/share/keyrings/githubcli-archive-keyring.gpg
+chmod go+r /usr/share/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" > /etc/apt/sources.list.d/github-cli.list
+apt-get update -y
+apt-get install -y gh
 
 echo "Installing dependencies..."
 npm install
@@ -16,4 +22,4 @@ VERSION=$(node -p "require('./package.json').version")
 TAG="v${VERSION}"
 
 echo "Uploading artifacts to GitHub release ${TAG}..."
-gh release upload "${TAG}" dist/*.AppImage dist/*.deb dist/*.rpm --clobber
+gh release upload "${TAG}" dist/*.AppImage dist/*.deb dist/*.rpm --repo rvmey/triggercmd-mission-control --clobber
